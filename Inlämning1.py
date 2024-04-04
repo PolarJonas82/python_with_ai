8803dde717bf0257ab743eea19bc4142b2dc9e7a
PolarJonas82 <jonas.ragneberg@hotmail.com>
2024-04-04 16:41:57 +0200

    stensaxpåse

---
+++ Inlämning1.py
@@ -0,0 +1,20 @@
+# sten sax påse spel
+
+spel = ["Sten", "Sax", "Påse"]
+playing = True
+input("Hej välkommen till Sten sax och påse spelet, för att starta tryck [1]")
+
+while playing == True:
+    print("Var gpd välj ett alternativ:")
+    print("1 Sten")
+    print("2 Sax")
+    print("3 Påse")
+    input()
+
+   
+        
+
+
+
+
+
