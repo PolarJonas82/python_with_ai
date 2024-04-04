042b912bf170f072f384299d8473d3ad334c2851
PolarJonas82 <jonas.ragneberg@hotmail.com>
2024-04-04 16:22:53 +0200

    tog bort

--- Jonas.py
+++
@@ -1,7 +0,0 @@
-a =6
-b = 7
-
-c = a+b
-print(c)
-print("Hej")
-print("då")
--- Test.py
+++
@@ -1,9 +0,0 @@
-name = input("what is your name? ")
-
-print("hello", name)
-
-print("your name reversed is ", name[::-1])
-
-print("bye", name)
-
-print ("Hej då")
