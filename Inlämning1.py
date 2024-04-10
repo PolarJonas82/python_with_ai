d40987d78bc029855542da71eae8e51130a6c507
PolarJonas82 <jonas.ragneberg@hotmail.com>
2024-04-10 08:10:27 +0200

    nytt

--- Inlämning1.py
+++ Inlämning1.py
@@ -7,8 +7,23 @@ spelare_poäng = 0
 motståndare_poäng = 0
 resultat = " "
 antal_omgångar = 3
+spela_igen = ""
 
-
+def Spela_Igen():
+    spela_igen = input("Vill du spela igen\nJa/Nej\n>").lower()
+        
+    if spela_igen == "ja":
+        motståndare_poäng = 0
+        spelare_poäng = 0
+        Clear()
+    else:
+        Clear()
+        print("Tack för den här gången!")
+        igång = False
+        
+    
+def Clear():
+    os.system('cls')
 
 def Vinst():
     resultat = "Du fick poäng"
@@ -20,17 +35,16 @@ def Förlust():
     return resultat
     
 
-os.system('cls')
-print("Hej välkommen till Sten sax och påse spelet!")
+Clear()
+print("Hej välkommen till Sten sax och påse spelet!\nFörst till 3 poäng vinner \nLycka till!!")
 
 
 while  igång:
     
     motståndare = random.choice(Motståndarens_val)
     
-    val = input("Var god välj nåt av alternativen: \n1:Sten \n2:Sax \n3:Påse \n> ")
-    val = val.lower()
-    os.system('cls')
+    val = input("Var god välj nåt av alternativen: \n1:Sten \n2:Sax \n3:Påse \n> ").lower()
+    Clear()
     
     #Vid oavgjort
     if val == motståndare:
@@ -39,11 +53,11 @@ while  igång:
     # vid vinst
     elif (val == "sten" and motståndare == "sax") or  (val == "sax" and motståndare == "påse") or ( val == "påse" and motståndare == "sten"):
         resultat = Vinst()
-        #print(resultat)
+        
         spelare_poäng +=1
 
     #Vid förlust
-    elif (val == "sten" and motståndare == "påse") or (val == "sax" and motståndare == "sten") or val == "påse" and motståndare == "sax":
+    elif (val == "sten" and motståndare == "påse") or (val == "sax" and motståndare == "sten") or (val == "påse" and motståndare == "sax"):
         resultat = Förlust()
         motståndare_poäng += 1
       
@@ -60,12 +74,40 @@ while  igång:
 
     if spelare_poäng == antal_omgångar:
         print("Du Vann!!")
-        igång = False
+       #Spela_Igen()
 
+
+        spela_igen = input("Vill du spela igen\nJa/Nej\n>").lower()
+        
+        if spela_igen == "ja":
+            motståndare_poäng = 0
+            spelare_poäng = 0
+            Clear()
+        else:
+            Clear()
+            print("Tack för den här gången!")
+            igång = False
+        
     elif motståndare_poäng == antal_omgångar:
         print("Du Förlorade!!")
-        igång = False
+        #Spela_Igen()
 
+        spela_igen = input("Vill du spela igen\nJa/Nej\n>").lower()
+        
+        if spela_igen == "ja":
+            motståndare_poäng = 0
+            spelare_poäng = 0
+            Clear()
+        else:
+            Clear()
+            print("Tack för den här gången!")
+            igång = False
+        
+        
+    
+        
+    
+        
     
 
 
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-Välkommen till sten sax och påse spelet
+Välkommen till sten sax och påse spelet!
 
-Först till 3 poäng vinner
+Först till 3 poäng vinner.
 När du startat spelet så ska du välja genom att skriva in sten sax eller påse(stora eller små bokstäver spelar ingen roll).
+och sen trycka enter.
 efter det slumpar datorn fram ett val.
 
 Sten vinner över sax.
